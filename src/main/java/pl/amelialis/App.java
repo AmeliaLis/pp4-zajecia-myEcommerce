c944e76a4253345b7f939850915a54759d7307bf
AmeliaLis <s221423@student.uek.krakow.pl>
2023-04-17 18:15:28 +0200

    spring application configuration

---
+++ src/main/java/pl/amelialis/App.java
@@ -0,0 +1,23 @@
+package pl.amelialis;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.amelialis.productcatalog.HashMapProductStorage;
+import pl.amelialis.productcatalog.ProductCatalog;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args){
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    ProductCatalog createProductCatalog(){
+        ProductCatalog productCatalog = new ProductCatalog(new HashMapProductStorage());
+
+
+        return productCatalog;
+    }
+}
